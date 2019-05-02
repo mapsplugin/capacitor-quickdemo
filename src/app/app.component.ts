af23f7a0d121c9817999e1226f1a866ed0e30691
masashiGMS <masashikatsumata.gms@gmail.com>
2019-05-01 18:11:30 -0700

    wip: (ios)marker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,9 +16,9 @@ export class AppComponent {
       icon: 'home'
     },
     {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
+      title: 'Marker',
+      url: '/marker',
+      icon: 'home'
     }
   ];
 
